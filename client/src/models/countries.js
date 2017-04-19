5db0b60e089468300dd6649b944a6b3bd3a757f0
Graeme Bell <bell.graeme@gmail.com>
2017-04-19 11:43:03 +0100

    Server.js countries.js started

---
+++ client/src/models/countries.js
@@ -0,0 +1,18 @@
+var Countries =  function(){};
+
+Countries.prototype = {
+
+  makeRequest: function(url, callback){
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload =callback;
+    request.send();
+  },
+
+  // makePostRequest: 
+
+}
+
+
+
+module.exports = Countries;
